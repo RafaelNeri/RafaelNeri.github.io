a07d66e4760acd8cbc939ea240b2bbd008fcc636
Rafael Neri Polatto <contato@rafaelneri.com>
2022-06-08 23:26:02 -0300

    Site Atualizado

--- doutrinas/js/custom.js
+++ doutrinas/js/custom.js
@@ -1,6 +1,8 @@
 $( document ).ready(function()
 {
-    $.getJSON('https://rafaelneri.github.io/doutrinas/json/doctrines.json', function(data)
+    // $.getJSON('https://rafaelneri.github.io/doutrinas/json/doctrines.json', function(data)
+    $.getJSON('https://novohinario.cpb.com.br/doutrinas/json/doctrines.json', function(data)
+
     {        
         data.doctrines.forEach(function(doctrine, i_doctrine){
             
@@ -16,7 +18,7 @@ $( document ).ready(function()
             $('<li class="dropdown nav-item text-center mt-1"><a class="nav-link dropdown-toggle" href="#'+doctrine.slug+'" id="navbarDropdown'+i_doctrine+'" role="button" data-bs-toggle="dropdown" aria-expanded="false">'+doctrine.name+'</a><ul class="dropdown-menu" aria-labelledby="navbarDropdown'+i_doctrine+'" id="catMobile'+i_doctrine+'"></ul></li>').appendTo("#menuMobileDoutrinas");
             
             // CATEGORIAS - BODY
-            $('<h2 id="'+doctrine.slug+'">'+doctrine.name+' <img src="images/ico-link-hover.svg" class="iconLink" alt="A Doutrina de Deus" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-custom-class="custom-tooltip" title="Copiar link"></h2><p class="lead text-white mb-5">'+doctrine.description+'</p><div id="detalheCat'+i_doctrine+'"></div>').appendTo("#doutrinasDetalhes");
+            $('<h2 name="'+doctrine.slug+'" id="'+doctrine.slug+'">'+doctrine.name+' <img src="images/ico-link-hover.svg" class="iconLink" id="tooltip-'+doctrine.slug+'" data-clipboard-text="'+window.location.href.split('#')[0]+'#'+doctrine.slug+'" alt="'+doctrine.name+'" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-custom-class="custom-tooltip" title="Copiar link"></h2><p class="lead text-white mb-5" style="text-align: justify;">'+doctrine.description+'</p><div id="detalheCat'+i_doctrine+'"></div>').appendTo("#doutrinasDetalhes");
             
             doctrine.belief.forEach(function(belief, i_belief){
                 // SUBCATEGORIAS - SIDEBAR
@@ -26,7 +28,7 @@ $( document ).ready(function()
                 $('<li><a class="dropdown-item" href="#'+belief.slug+'">'+belief.name+'</a></li>').appendTo('#catMobile'+i_doctrine);
                 
                 // SUBCATEGORIAS - BODY
-                $('<h3 class="text-white" id="'+belief.slug+'">'+belief.name+' <img src="images/ico-link-hover.svg" class="iconLink" alt="'+belief.name+'" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-custom-class="custom-tooltip" title="Copiar link"></h3><p class="lead text-white">Crença Fundamental (Hinos de '+belief.firstHymn+' a '+belief.lastHymn+')</p><p class="text-white mb-3">'+belief.description+'</p><table class="table table-dark table-hover" style="margin-bottom: 100px;"><thead><tr><th scope="col" class="text-center" style="width: 15%;">Número</th><th scope="col">Hino</th><th scope="col" class="text-center" style="width: 15%;">Vídeo</th></tr></thead><tbody id="hinosCat'+i_doctrine+'Sub'+i_belief+'"></tbody></table>').appendTo('#detalheCat'+i_doctrine);
+                $('<h3 class="text-white" name="'+belief.slug+'" id="'+belief.slug+'">'+belief.name+' <img src="images/ico-link-hover.svg" class="iconLink" id="tooltip-'+belief.slug+'" data-clipboard-text="'+window.location.href.split('#')[0]+'#'+belief.slug+'" alt="'+belief.name+'" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-custom-class="custom-tooltip" title="Copiar link"></h3><p class="lead text-white">Hinos de '+belief.firstHymn+' a '+belief.lastHymn+'</p><p class="text-white mb-3" style="text-align: justify;">'+belief.description+'</p><table class="table table-dark table-hover" style="margin-bottom: 100px;"><thead><tr><th scope="col" class="text-center" style="width: 15%;">Número</th><th scope="col">Hino</th><th scope="col" class="text-center" style="width: 15%;">Vídeo</th></tr></thead><tbody id="hinosCat'+i_doctrine+'Sub'+i_belief+'"></tbody></table>').appendTo('#detalheCat'+i_doctrine);
                 
                 // HINOS
                 belief.hymns.forEach(function(hymn, i_hymn){
@@ -40,5 +42,25 @@ $( document ).ready(function()
             });
             
         });
+        
+        var hash = window.location.hash;
+        
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(hash)?.offset()?.top
+        }, 300);
+        
+        var clipboard = new ClipboardJS('.iconLink');
+        
+        $('img').tooltip();
+
+        clipboard.on('success', function(e) {
+            const tooltip = bootstrap.Tooltip.getInstance('#'+e.trigger.id);
+            tooltip.setContent({ '.tooltip-inner': 'Link copiado!' });
+            setTimeout(function() { 
+                tooltip.setContent({ '.tooltip-inner': 'Copiar link' });
+            }, 2000);
+        });
+        
+        
     });
 });
